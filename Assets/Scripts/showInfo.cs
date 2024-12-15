d3a76f14be596ca9b6a82b4cb65f6f1167362f72
An Long <113451791+anlongvrdev@users.noreply.github.com>
2024-12-15 23:41:24 +0700

    new changes

--- Assets/Scripts/showInfo.cs
+++ Assets/Scripts/showInfo.cs
@@ -11,15 +11,28 @@ public class showInfo : MonoBehaviour
     public void onShow1()
     {
         plane1.SetActive(true);
+        plane2.SetActive(false);
+        plane3.SetActive(false);
     }
 
     public void onShow2()
     {
+        plane1.SetActive(false);
         plane2.SetActive(true);
+        plane3.SetActive(false);
     }
 
     public void onShow3() 
     {
+        plane1.SetActive(false);
+        plane2.SetActive(false);
         plane3.SetActive(true);
     }
+
+    public void onOff()
+    {
+        plane1?.SetActive(false);
+        plane2.SetActive(false);
+        plane3.SetActive(false);
+    }    
 }
